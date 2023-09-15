425dc883167f93aff6373f3167110394657a0d9f
huynhnguyen-haku <huynhnguyen1532@gmail.com>
2023-09-15 08:52:51 +0700

    First Commit

---
+++ SuperDataWorker/src/main/java/com/example/superdataworker/SuperDataWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.example.superdataworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperDataWorkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperDataWorkerApplication.class, args);
+    }
+
+}
