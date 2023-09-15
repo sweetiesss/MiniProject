7e1ee8e1b2c296f5cc466c6facf01bb9f0edac97
huynhnguyen-haku <huynhnguyen1532@gmail.com>
2023-09-15 09:43:59 +0700

    First Commit

---
+++ SuperDataWorker/src/test/java/com/example/superdataworker/SuperDataWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.superdataworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperDataWorkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
