f072806aeb81e37410dec3d9c47d1544f4c50d01
sweetiesss <ahreynohope@gmail.com>
2023-09-21 15:13:48 +0700

    Update API Version 2 - log4j2 log file now working

--- demo1/src/main/java/com/example/demo1/Demo1Application.java
+++ demo1/src/main/java/com/example/demo1/Demo1Application.java
@@ -1,5 +1,7 @@
 package com.example.demo1;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Demo1Application {
 
     public static void main(String[] args) {
+        final Logger logger = LogManager.getLogger("log4j2Logger");
         SpringApplication.run(Demo1Application.class, args);
     }
-
 }
